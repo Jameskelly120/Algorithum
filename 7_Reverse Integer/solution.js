66746a359382b63b741c4f6ed4a3e0e445416457
James-kelly120 <yonsin120@outlook.com>
2020-01-14 17:27:42 -0800

    first

---
+++ 7_Reverse Integer/solution.js
@@ -0,0 +1,6 @@
+var reverse = function(x) {
+    let reversedNumber = parseInt(x.toString().split('').reverse().join(''));
+    
+    if (reversedNumber > Math.pow(2,31) - 1) return 0;
+    return Math.sign(x) * reversedNumber;
+};
